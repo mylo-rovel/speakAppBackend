c5536fcc19e37228630c0464b61b9488c5c09af2
Emilio Rojas Veliz <rovel.code@outlook.com>
2022-12-04 00:25:31 -0300

    features(project): add better docker compose and better oauth modular code style

--- src/routes/oauth/oauth.router.ts
+++
@@ -1,7 +0,0 @@
-import express, {Router} from "express";
-
-export const oauthRouter: Router = express.Router();
-
-oauthRouter.get("/");
-oauthRouter.get("/callback");
-oauthRouter.get("/logout");
--- src/routes/pokemon/pokemon.router.ts
+++
@@ -1,8 +0,0 @@
-import express, {Router} from "express";
-import { 
-    httpGetRandomPokemon
- } from "./pokemon.controller.js";
-
-export const pokemonRouter: Router = express.Router();
-
-pokemonRouter.get("/random", httpGetRandomPokemon);
